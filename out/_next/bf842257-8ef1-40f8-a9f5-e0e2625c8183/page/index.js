f3129df3f7b09079a0bcc145a11ff3f58907b880
Thierry Charbonnel <thierryc@users.noreply.github.com>
2017-11-13 09:58:58 -0500

    Initial out subtree commit gh-pages

---
+++ out/_next/bf842257-8ef1-40f8-a9f5-e0e2625c8183/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([2],[],[211]);
+            return { page: comp.default }
+          })
+        
